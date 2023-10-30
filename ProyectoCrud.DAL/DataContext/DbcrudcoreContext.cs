01da4ebe8fbedb796115dd52d556efa4d1ae3f27
KimyFuchs <Karim.u506203@gmail.com>
2023-10-30 12:02:54 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoCrud.DAL/DataContext/DbcrudcoreContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ProyectoCrud.Models;
+
+namespace ProyectoCrud.DAL.DataContext;
+
+public partial class DbcrudcoreContext : DbContext
+{
+    public DbcrudcoreContext()
+    {
+    }
+
+    public DbcrudcoreContext(DbContextOptions<DbcrudcoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contacto>(entity =>
+        {
+            entity.HasKey(e => e.IdContacto).HasName("PK__Contacto__A4D6BBFA50408D5F");
+
+            entity.ToTable("Contacto");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
